91e5ea2b7fcb447405a4100cb285b47061b6064f
iktushar01 <iktushar2020@gmail.com>
2026-03-17 20:01:05 +0600

    Creating Zod Validation Schema, Auth Types For Login

---
+++ src/app/(commonLayout)/(auth)/login/_action.ts
@@ -0,0 +1,11 @@
+import { ILoginPayload } from "@/zod/auth.validation";
+
+export const loginAction = async (payload : ILoginPayload) => {
+
+    try {
+        
+    } catch (error) {
+        
+    }
+    
+}
