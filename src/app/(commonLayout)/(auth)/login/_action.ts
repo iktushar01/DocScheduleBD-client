02ed059f02a5e0ca5195c3a6714c2cdd22c58811
iktushar01 <iktushar2020@gmail.com>
2026-03-17 22:19:26 +0600

    Creating Server Action For Login

--- src/app/(commonLayout)/(auth)/login/_action.ts
+++ src/app/(commonLayout)/(auth)/login/_action.ts
@@ -1,11 +1,30 @@
-import { ILoginPayload } from "@/zod/auth.validation";
+"use server";
 
-export const loginAction = async (payload : ILoginPayload) => {
+import { ILoginPayload, loginZodSchema } from "@/zod/auth.validation";
+import { ILoginResponse } from "@/types/auth.types";
+import { httpClient } from "@/lib/axios/httpClient";
+import { ApiErrorResponse } from "@/types/api.types";
 
+export const loginAction = async (payload : ILoginPayload) : Promise<ILoginResponse | ApiErrorResponse> => {
+    const parsedPayload = loginZodSchema.safeParse(payload);
+    if(!parsedPayload.success){
+        const firstError = parsedPayload.error.issues[0].message || "Invalid credentials";
+        return {
+            success : false,
+            message : firstError,
+        }
+    }
     try {
-        
+        const response = await httpClient.post<ILoginResponse>('/auth/login', parsedPayload.data);
+
+        const {token, accessToken, refreshToken, user} = response.data;
+
+        return response.data;
     } catch (error) {
-        
+        return {
+            success : false,
+            message : `login failed : ${error}`,
+        }
     }
     
 }
